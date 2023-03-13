c0978c20c60b8b6188a4ab5eb51483e4ba327337
Sander Vonk <sander.c.vonk@gmail.com>
2023-03-13 10:25:46 -0700

    Update CalendarBlock.vue

--- src/components/Portal/CalendarBlock.vue
+++ src/components/Portal/CalendarBlock.vue
@@ -13,6 +13,9 @@ export default {
 
 <style scoped>
 main.calendar {
+  width: 100%;
+  max-width: 700px;
+  margin: 0 auto;
 }
 /* use a ::before or ::after to force it to a 4:3 ratio */
 main.calendar::before {
