348c344bba55ac580bd3462965a371a833b41b96
Sander Vonk <10799950+sandervonk@users.noreply.github.com>
2023-03-20 17:18:46 -0700

    fix icons

--- vue.config.js
+++ vue.config.js
@@ -3,4 +3,43 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   configureWebpack: {},
   transpileDependencies: true,
+  pwa: {
+    themeColor: "#f5c14b",
+    msTileColor: "#f5c14b",
+    appleMobileWebAppCapable: "yes",
+    // theme top bar to match theme color
+    appleMobileWebAppStatusBarStyle: "black-translucent",
+    name: "MV Test Tracker",
+    short_name: "MVTT",
+    start_url: "/",
+    display: "standalone",
+    shortcuts: [
+      {
+        name: "Home",
+        short_name: "Home",
+        description: "Go to the home page",
+        url: "/",
+        icons: [
+          {
+            src: "/img/icons/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+        ],
+      },
+      {
+        name: "Portal",
+        short_name: "Portal",
+        description: "Open the portal",
+        url: "/portal",
+        icons: [
+          {
+            src: "/img/icons/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+        ],
+      },
+    ],
+  },
 });
