62b9d4eb119a7143d189514680365f7e367d7d14
Sander Vonk <10799950+sandervonk@users.noreply.github.com>
2023-03-22 08:52:04 -0700

    fix pwa

--- vue.config.js
+++ vue.config.js
@@ -4,42 +4,121 @@ module.exports = defineConfig({
   configureWebpack: {},
   transpileDependencies: true,
   pwa: {
-    themeColor: "#f5c14b",
+    themeColor: "",
     msTileColor: "#f5c14b",
     appleMobileWebAppCapable: "yes",
-    // theme top bar to match theme color
     appleMobileWebAppStatusBarStyle: "black-translucent",
-    name: "MV Test Tracker",
-    short_name: "MVTT",
-    start_url: "/",
     display: "standalone",
-    shortcuts: [
-      {
-        name: "Home",
-        short_name: "Home",
-        description: "Go to the home page",
-        url: "/",
-        icons: [
-          {
-            src: "/img/icons/android-chrome-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-        ],
-      },
-      {
-        name: "Portal",
-        short_name: "Portal",
-        description: "Open the portal",
-        url: "/portal",
-        icons: [
-          {
-            src: "/img/icons/android-chrome-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-        ],
-      },
-    ],
+    manifestOptions: {
+      name: "MV Test Tracker",
+      short_name: "MVTT",
+      description:
+        "MV Test tracker provides an effective way to manage your upcoming tests and exams.",
+      screenshots: [
+        // add screenshots
+      ],
+      shortcuts: [
+        {
+          name: "Home",
+          short_name: "Home",
+          description: "Go to the home page",
+          url: "/",
+          icons: [
+            {
+              src: "/img/icons/android-chrome-192x192.png",
+              sizes: "192x192",
+              type: "image/png",
+            },
+          ],
+        },
+        {
+          name: "Portal",
+          short_name: "Portal",
+          description: "Open the portal",
+          url: "/portal",
+          icons: [
+            {
+              src: "/img/icons/android-chrome-192x192.png",
+              sizes: "192x192",
+              type: "image/png",
+            },
+          ],
+        },
+      ],
+      icons: [
+        {
+          src: "./img/icons/android-chrome-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "./img/icons/android-chrome-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+        {
+          src: "./img/icons/android-chrome-maskable-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+          purpose: "maskable",
+        },
+        {
+          src: "./img/icons/android-chrome-maskable-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "maskable",
+        },
+        {
+          src: "./img/icons/apple-touch-icon-60x60.png",
+          sizes: "60x60",
+          type: "image/png",
+        },
+        {
+          src: "./img/icons/apple-touch-icon-76x76.png",
+          sizes: "76x76",
+          type: "image/png",
+        },
+        {
+          src: "./img/icons/apple-touch-icon-120x120.png",
+          sizes: "120x120",
+          type: "image/png",
+        },
+        {
+          src: "./img/icons/apple-touch-icon-152x152.png",
+          sizes: "152x152",
+          type: "image/png",
+        },
+        {
+          src: "./img/icons/apple-touch-icon-180x180.png",
+          sizes: "180x180",
+          type: "image/png",
+        },
+        {
+          src: "./img/icons/apple-touch-icon.png",
+          sizes: "180x180",
+          type: "image/png",
+        },
+        {
+          src: "./img/icons/favicon-16x16.png",
+          sizes: "16x16",
+          type: "image/png",
+        },
+        {
+          src: "./img/icons/favicon-32x32.png",
+          sizes: "32x32",
+          type: "image/png",
+        },
+        {
+          src: "./img/icons/msapplication-icon-144x144.png",
+          sizes: "144x144",
+          type: "image/png",
+        },
+        {
+          src: "./img/icons/mstile-150x150.png",
+          sizes: "150x150",
+          type: "image/png",
+        },
+      ],
+    },
   },
 });
