c246be7b638b8f3baca656e1e67de17460b9a26c
Sander Vonk <10799950+sandervonk@users.noreply.github.com>
2023-03-25 19:15:09 -0700

    prep for electron.js

---
+++ src/components/Electron/MacTitlebar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="mac-titlebar">
+    <div class="title">
+      {{ title }}
+    </div>
+    <div class="buttons">
+      <button class="minimize" @click="minimize">-</button>
+      <button class="maximize" @click="maximize">&#8597;</button>
+      <button class="close" @click="close">&times;</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MacTitleBar",
+  props: {
+    title: {
+      type: String,
+      default: "",
+    },
+  },
+  methods: {
+    minimize() {
+      window.api.send("minimize");
+    },
+    maximize() {
+      window.api.send("maximize");
+    },
+    close() {
+      window.api.send("close");
+    },
+  },
+};
+</script>
---
+++ src/components/Electron/WindowsTitlebar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="windows-titlebar">
+    <div class="title">
+      {{ title }}
+    </div>
+    <div class="buttons">
+      <button class="minimize" @click="minimize">&#xE921;</button>
+      <button class="maximize" @click="maximize">&#xE922;</button>
+      <button class="close" @click="close">&times;</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WindowsTitleBar",
+  props: {
+    title: {
+      type: String,
+      default: "",
+    },
+  },
+  methods: {
+    minimize() {
+      window.api.send("minimize");
+    },
+    maximize() {
+      window.api.send("maximize");
+    },
+    close() {
+      window.api.send("close");
+    },
+  },
+};
+</script>
