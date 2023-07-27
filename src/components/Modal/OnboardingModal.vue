7db22c2030d25ce712c56f5c0a8ab7882db852bf
Sander Vonk <10799950+sandervonk@users.noreply.github.com>
2023-07-26 22:04:41 -0700

    work on signup form

--- src/components/Modal/OnboardingModal.vue
+++ src/components/Modal/OnboardingModal.vue
@@ -1,13 +1,35 @@
 <template>
-  <main class="onboardng" ref="modal">
+  <main class="onboardng" ref="modal" id="join_modal">
     <div class="overlay_contents_wrapper nopad">
       <img class="modal_art_part contrast_small_dark" v-lazy="source" />
-      <div class="overlay_contents"></div>
+      <div class="overlay_contents pad_overlay overlay_more_pad_horizontal">
+        <h2 class="modal_header_title overlay_title">Join the MVTT Beta</h2>
+        <div class="overlay_inputs inputs_column">
+          <input
+            class="styled_input styled_obj"
+            type="name"
+            v-model="form.name"
+            placeholder="Your Name"
+          />
+          <input
+            class="styled_input styled_obj"
+            type="email"
+            v-model="form.email"
+            placeholder="Your @mvla.net Email"
+          />
+          <textarea
+            v-model="form.usage"
+            class="styled_input styled_textarea styled_obj"
+            type="text"
+            placeholder="How will you use MVTT (optional)"
+          />
+        </div>
+      </div>
     </div>
     <div class="bottom_actions">
       <button class="close_action" @click="$emit('close')">Close</button>
       <div class="flex_spacer"></div>
-      <button class="continue_action" disabled="">Continue to Sign-Up</button>
+      <button class="continue_action" :disabled="!contents_ready">Continue to Sign-Up</button>
     </div>
   </main>
 </template>
@@ -18,9 +40,23 @@ import "@/views/Portal/overlay.css";
 export default {
   emits: ["close"],
   data() {
-    return {};
+    return {
+      form: {
+        name: "",
+        email: "",
+        usage: "",
+      },
+    };
   },
   computed: {
+    contents_ready() {
+      // filled out name and email ending in @mvla.net
+      return (
+        this.form.name.length > 0 &&
+        this.form.email.length > 0 &&
+        this.form.email.endsWith("@mvla.net")
+      );
+    },
     store() {
       return useMainStore();
     },
@@ -41,9 +77,16 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+main#join_modal {
+  max-width: 700px;
+}
 .overlay_contents {
   border: none;
+  justify-content: center;
+  align-items: stretch;
+  display: flex;
+  flex-flow: column;
 }
 .bottom_actions {
   border-top: solid var(--thickness-overlay-border) var(--color-overlay-border);
@@ -55,10 +98,27 @@ export default {
   align-items: stretch;
   max-height: 100%;
 }
+h2.overlay_title {
+  text-align: center;
+  font-weight: 700;
+  margin-bottom: 20px;
+}
 .modal_art_part {
   height: 400px;
   width: 270px;
   object-fit: scale-down;
   object-position: bottom left;
+}
+.inputs_column {
+  display: flex;
+  flex-direction: column;
+  justify-content: stretch;
+  align-items: stretch;
+  margin: 0;
+}
+.inputs_column .styled_input {
+  width: 100%;
+  margin: 0;
+  margin-bottom: calc(var(--padding-overlay) / 2);
 }
 </style>
