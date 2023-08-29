d8103a4754d8348e4cdf3082e6f542589e661cb6
Sander Vonk <10799950+sandervonk@users.noreply.github.com>
2023-08-28 21:23:52 -0700

    enable surveys for teachers

--- src/store/index.js
+++ src/store/index.js
@@ -263,8 +263,8 @@ export const useMainStore = defineStore({
      */
     done_daily_survey() {
       if (!this.active_doc) return false;
-      if (this.is_teacher) return true;
-      return this.active_doc.done_surveys?.includes(today);
+      // if (this.is_teacher) return true;
+      return this.active_doc?.done_surveys && this.active_doc.done_surveys?.includes(today);
     },
     /**
      * @function done_join_form
