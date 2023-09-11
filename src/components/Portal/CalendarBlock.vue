60d90f29d8d9095ea0c4b55aeea48f19ed1ee92f
Sander Vonk <10799950+sandervonk@users.noreply.github.com>
2023-09-10 22:07:43 -0700

    fix date move

--- src/components/Portal/CalendarBlock.vue
+++ src/components/Portal/CalendarBlock.vue
@@ -241,7 +241,7 @@ export default {
         this.store
           .update_task(this.drag.task.ref, {
             ...this.drag.task,
-            date: to.toISOString().split("T")[0],
+            date: to,
           })
           .then(() => {
             new SuccessToast(
