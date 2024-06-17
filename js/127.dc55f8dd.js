c168b774485c8ba4a97b953faa6e4a7bcc99c238
Sander Vonk <10799950+sandervonk@users.noreply.github.com>
2024-06-16 20:48:54 -0700

    Updates

--- js/127.dc55f8dd.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkmvtime"]=self["webpackChunkmvtime"]||[]).push([[127],{127:function(e,r,s){s.r(r),s.d(r,{default:function(){return d}});var c=s(6768);const n={class:"userteacher"},t=(0,c.Lk)("div",{class:"placeholder"},"Users & Teachers",-1),u=[t];function a(e,r,s,t,a,i){return(0,c.uX)(),(0,c.CE)("div",n,u)}var i={},l=s(1241);const v=(0,l.A)(i,[["render",a]]);var d=v}}]);
-//# sourceMappingURL=127.dc55f8dd.js.map
